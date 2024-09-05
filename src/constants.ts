815055251152e1b33ac1b4d8af2a32aed41618ad
Tyus Durant <contact@tyusdurant.com>
2024-09-05 13:28:58 -0400

    end tut

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION = "Article, Stories & Tutorials for Tech People";
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLE_PER_PAGE = 6;
